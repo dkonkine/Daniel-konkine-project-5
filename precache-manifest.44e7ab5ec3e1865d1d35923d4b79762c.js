300bd5236bc103ee1c05b8a66a9d5da26c8a94d2
DanielKonkine <dkonkine@yahoo.ca>
2020-08-10 16:42:07 -0400

    Updates

---
+++ precache-manifest.44e7ab5ec3e1865d1d35923d4b79762c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edaf37d5cca61ca6e787317bdba794a2",
+    "url": "/Daniel-konkine-project-5/index.html"
+  },
+  {
+    "revision": "163c420c7f351266e761",
+    "url": "/Daniel-konkine-project-5/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b62b09b183ee92e84e55",
+    "url": "/Daniel-konkine-project-5/static/js/2.32eb91f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Daniel-konkine-project-5/static/js/2.32eb91f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "163c420c7f351266e761",
+    "url": "/Daniel-konkine-project-5/static/js/main.d4a6443c.chunk.js"
+  },
+  {
+    "revision": "c8a212752fca8d9a4bef",
+    "url": "/Daniel-konkine-project-5/static/js/runtime-main.fc2804ac.js"
+  }
+]);
